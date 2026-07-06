e6044a5c9a4f31fddf25762bd765300a42832841
Michele Stanzani <michele.stanzani2@studio.unibo.it>
2026-07-06 23:49:18 +0200

    added pawn moves

--- src/main/java/scacchi/model/pieces/Pawn.java
+++ src/main/java/scacchi/model/pieces/Pawn.java
@@ -1,16 +1,63 @@
 package scacchi.model.pieces;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import scacchi.model.board.Position;
+import scacchi.model.board.ReadOnlyBoard;
+
 public class Pawn implements Piece {
 
-    private final boolean isWhite;
+    private final int color; // 1 for white, -1 for black
 
-    public Pawn(boolean isWhite) {
-        this.isWhite = isWhite;
+    public Pawn(final int color) {
+        if (color != 1 && color != -1) {
+            throw new IllegalArgumentException("Color must be 1 (white) or -1 (black)");
+        }
+        this.color = color;
     }
 
     @Override
     public char getFenChar() {
-        return isWhite ? 'P' : 'p';
+        return this.color == 1 ? 'P' : 'p';
     }
     
+    @Override
+    public int getColor() {
+        return this.color;
+    }
+
+    @Override
+    public Set<Position> getValidMoves(final Position currentPosition, final ReadOnlyBoard board) {
+        Set<Position> validMoves = new HashSet<>();
+        int direction = this.color; // 1 for white (up), -1 for black (down)
+        int startRow = this.color == 1 ? 1 : 6; // Starting row for pawns
+
+        final int nextY = currentPosition.y() + direction;
+
+        if (nextY >= 0 && nextY < 8) {
+            final Position forwardPos = new Position(currentPosition.x(), nextY);
+            if (board.isEmpty(forwardPos)) {
+                validMoves.add(forwardPos);
+
+                if (currentPosition.y() == startRow) {
+                    final Position doubleForwardPos = new Position(currentPosition.x(), currentPosition.y() + 2 * direction);
+                    if (board.isEmpty(doubleForwardPos)) {
+                        validMoves.add(doubleForwardPos);
+                    }
+                }
+            }
+            final int[] captureX = {currentPosition.x() - 1, currentPosition.x() + 1}; 
+            for (int x : captureX) {
+                if (x >= 0 && x < 8) {
+                    final Position targetPos = new Position(x, nextY);
+                    final Optional<Piece> targetPiece = board.getPieceAt(targetPos);
+                    if (targetPiece.isPresent() && targetPiece.get().getColor() != this.color) {
+                        validMoves.add(targetPos);
+                    }
+                }
+            }
+        }
+        return validMoves;
+    }
 }
