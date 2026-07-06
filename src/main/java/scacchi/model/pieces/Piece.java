35b28fa89261a858fd325c2d1e518b74b47ee1b3
Michele Stanzani <michele.stanzani2@studio.unibo.it>
2026-07-06 22:06:33 +0200

    added these functions:getFenChar, getColor, getvalidMoves

--- src/main/java/scacchi/model/pieces/Piece.java
+++ src/main/java/scacchi/model/pieces/Piece.java
@@ -1,9 +1,11 @@
 package scacchi.model.pieces;
 
+import java.util.Set;
+import scacchi.model.board.Position;
+import scacchi.model.board.ReadOnlyBoard;
 /**
  * Interface, representing a generic chess piece.
  */
-@FunctionalInterface
 public interface Piece {
 
     /**
@@ -13,4 +15,20 @@ public interface Piece {
      * @return FEN character
      */
     char getFenChar();
+
+        /**
+     * Returns the color of the piece.
+     *
+     * @return '1' for white, '-1' for black
+     */
+    int getColor();
+
+        /**
+     * Calculates all valid moves for this piece given its current position and the board state.
+     *
+     * @param currentPosition the current position of the piece
+     * @param board the read-only state of the board
+     * @return a Set of valid destination positions
+     */
+    Set<Position> getValidMoves(Position currentPosition, ReadOnlyBoard board);
 }
